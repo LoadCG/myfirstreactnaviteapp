52a8b9ef4ad1d8f9f5938778c733ff775a749887
Cauan Gabriel <cauangabrielresende@gmail.com>
2024-01-31 17:07:01 -0300

    Update index.js

--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from "react-native"
+import { TouchableOpacity, StyleSheet, Text, View } from "react-native"
 import PrimeVideoLogo from "../../components/assets/prime_video.png";
 import AmazonLogo from "../../components/assets/amazon_logo.png";
 
@@ -9,6 +9,20 @@ export const Home = () => {
       <Image style={styles.primeLogoImg} source={PrimeVideoLogo} />
       <Image style={styles.amazonLogoImg} source={AmazonLogo} />
 
+    </View>
+    <View style={styles.category}>
+      <TouchableOpacity>
+        <Text style={styles.categoryText}>Home</Text>
+      </TouchableOpacity>
+      <TouchableOpacity>
+        <Text style={styles.categoryText}>TV Shows</Text>
+      </TouchableOpacity>
+      <TouchableOpacity>
+        <Text style={styles.categoryText}>Movies</Text>
+      </TouchableOpacity>
+      <TouchableOpacity>
+        <Text style={styles.categoryText}>Kids</Text>
+      </TouchableOpacity>
     </View>
   </View>
 }
@@ -21,6 +35,27 @@ const styles = StyleSheet.create({
   },
 
   header: {
+    width: "100%",
+    paddingTop: 80,
+    alignItems: center,
+    justifyContent: center,
+  },
 
+  amazonLogoImg: {
+    marginTop: -32,
+    marginLeft: 30,
+  },
+
+  category: {
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    marginTop: 30,
+    marginBottom: 15,
+  },
+
+  categoryText: {
+    fontSize: 14,
+    fontWeight: "700",
+    color: "#ffffff",
   },
 });
